9c3698cd761b2f9bca483e0c31acfa0e21a058e9
PraneethSimon12 <praneethsimon2@gmail.com>
2024-06-12 13:23:17 +0530

    adfv

--- core/home/views.py
+++ core/home/views.py
@@ -1,11 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def index(request):
     courses = {
         'course_name' : 'Python',
         'names' : ['django','flask','Tornado','fastapi'],
         'course_provider' : 'scaler'
     }
+    if request.method == 'GET':
+        print('Get request hit')
+    elif request.method == "POST":
+        data = request.data
+        print('****')
+        print(data)
+        print('****')
+        print('Post request hit')
     return Response(courses)
