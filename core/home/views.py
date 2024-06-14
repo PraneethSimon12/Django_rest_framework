55438836dda656f6be41cdb80bd5c02876f239ca
PraneethSimon12 <praneethsimon2@gmail.com>
2024-06-14 13:22:08 +0530

    dsfv

--- core/home/views.py
+++ core/home/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from home.models import Person
+from rest_framework import status
+from home.serializers import PeopleSerializer
 
 @api_view(['GET','POST'])
 def index(request):
@@ -17,3 +20,19 @@ def index(request):
         print('****')
         print('Post request hit')
     return Response(courses)
+
+@api_view(['GET','POST'])
+def person(request):
+    if request.method == 'GET':
+        objs = Person.objects.all()
+        serializer = PeopleSerializer(objs,many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'POST':
+        data = request.data
+        serializer = PeopleSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+
